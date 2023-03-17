372ac2cd9a66b3668127f5ee97390fc3870bc784
Noam <noammolloy@gmail.com>
2023-03-17 12:21:51 -0400

    fixed

--- ca01/gpt.py
+++ ca01/gpt.py
@@ -18,11 +18,6 @@ On Windows:
 % python gpt.py
 '''
 import openai
-<<<<<<< HEAD
-# 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
-=======
-# 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
->>>>>>> d24f36a586645cf9b70dd607dfdb5122ca0e7347
 
 class GPT():
     ''' make queries to gpt from a given API '''
@@ -30,12 +25,7 @@ class GPT():
         ''' store the apikey in an instance variable '''
         self.apikey=apikey
         # Set up the OpenAI API client
-<<<<<<< HEAD
-        openai.api_key = "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" #os.environ.get('APIKEY')
-=======
-        openai.api_key = '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'
- #os.environ.get('APIKEY')
->>>>>>> 6730a5e03122d6a7e30976c8f5c868bf853913aa
+        openai.api_key = apikey #os.environ.get('APIKEY')
 
         # Set up the model and prompt
         self.model_engine = "text-davinci-003"
@@ -72,13 +62,5 @@ if __name__=='__main__':
     '''
     '''
     import os
-<<<<<<< HEAD
-    g = GPT(os.environ.get("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"))
-=======
-<<<<<<< HEAD
-    g = GPT(os.environ.get("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"))
-=======
-    g = GPT(os.environ.get("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"))
->>>>>>> 6730a5e03122d6a7e30976c8f5c868bf853913aa
->>>>>>> d24f36a586645cf9b70dd607dfdb5122ca0e7347
+    g = GPT(os.environ.get("APIKEY"))
     print(g.getResponse("what does openai's GPT stand for?"))
