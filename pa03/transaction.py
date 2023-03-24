02d389d679278dcd9272924e8445f25e9d982074
jingyiwang21 <jingyiwang@brandeis.edu>
2023-03-24 14:30:13 -0400

    added table

--- pa03/transaction.py
+++ pa03/transaction.py
@@ -2,5 +2,8 @@ import sqlite3
 
 class Transaction:
     def __init__(self, filename) -> None:
-        pass
-
+        con= sqlite3.connect(filename)
+        cur = con.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS transactions (id INTEGER PRIMARY KEY, item# int, amount int, category text, date text, description text)")
+        con.commit()
+        con.close()
