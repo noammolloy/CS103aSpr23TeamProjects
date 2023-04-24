5e31b000c100a390bee2c613529c811d0d164009
Noam <noammolloy@gmail.com>
2023-04-23 21:06:20 -0400

    ansjkdan

--- pa04/firstapp/routes/transactions.js
+++ pa04/firstapp/routes/transactions.js
@@ -25,7 +25,24 @@ isLoggedIn = (req,res,next) => {
 router.get('/transactions/',
   isLoggedIn,
   async (req, res, next) => {
-            res.render('transactions');
+      let items = await TransactionItem.find({userId:req.user._id})
+                                        .sort({completed:1,priority:1,createdAt:1})
+
+      res.render('transactions', items);
+});
+
+/* add the value in the body to the list associated to the key */
+router.post('/transactions',
+  isLoggedIn,
+  async (req, res, next) => {
+      const transaction = new TransactionItem(
+        {description: req.body.desc,
+         amount: req.body.amt,
+         category: req.body.cat,
+         date: new Date()
+        })
+      await transaction.save();
+      res.redirect('/transactions')
 });
 
 module.exports = router;
