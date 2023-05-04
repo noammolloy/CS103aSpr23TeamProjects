fa083abfa50e47ec54fb6f50f2b4dd7601e680c5
Noam <noammolloy@gmail.com>
2023-05-04 15:59:29 -0400

    ajaksnd

--- ca02/firstapp/routes/jingyi.js
+++ ca02/firstapp/routes/jingyi.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User')
+const axios = require('axios')
 
 isLoggedIn = (req,res,next) => {
     if (res.locals.loggedIn) {
@@ -13,7 +14,34 @@ isLoggedIn = (req,res,next) => {
 router.get('/jingyi/',
   isLoggedIn,
   async (req, res, next) => {
-            res.render('jingyi');
+            const r = "";
+            res.render('jingyi', {r});
 });
+
+router.post('/jingyi',
+  isLoggedIn,
+  async (req, res, next) => {
+      const prompt = "Change the following text to be written as if you are talking to a dinosaur: ".concat(req.body.prompt);
+      const response = await axios.post('https://api.openai.com/v1/engines/text-davinci-003/completions', {
+        prompt: prompt,
+        max_tokens: 3900,
+        
+      }, {
+        headers: {
+          'Authorization': `Bearer ${process.env.APIKEY}`,
+        },
+      });
+      const r = clean(response.data.choices[0].text.trim());
+      console.log(r)
+      res.render('jingyi', {r})
+});
+
+function clean(str) {
+  const index = str.indexOf('\n');
+  if (index === -1) {
+    return null; // return null if no newline is found
+  }
+  return str.slice(index + 1);
+}
 
 module.exports = router;
--- ca02/firstapp/routes/sydney.js
+++ ca02/firstapp/routes/sydney.js
@@ -21,19 +21,27 @@ router.get('/sydney/',
 router.post('/sydney',
   isLoggedIn,
   async (req, res, next) => {
-      const prompt = "Create a short story using these input keywords about dinosaurs: ".concat(req.body.prompt);
+      const prompt = "Create a short story under 200 words using these input keywords about dinosaurs: ".concat(req.body.prompt);
       const response = await axios.post('https://api.openai.com/v1/engines/text-davinci-003/completions', {
         prompt: prompt,
-        max_tokens: 200,
-        n: 1,
-        stop: '###',
+        max_tokens: 3900,
+        
       }, {
         headers: {
           'Authorization': `Bearer ${process.env.APIKEY}`,
         },
       });
-      const r = response.data.choices[0].text.trim();
+      const r = clean(response.data.choices[0].text.trim());
+
       res.render('sydney', {r})
 });
+
+function clean(str) {
+  const index = str.indexOf('\n');
+  if (index === -1) {
+    return null; // return null if no newline is found
+  }
+  return str.slice(index + 1);
+}
 
 module.exports = router;
